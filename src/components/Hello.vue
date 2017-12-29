349a6bc71c7d666ae7e2665692417d4b853061c2
Rob Clarke <leon@moringamutual.org>
2017-12-29 10:39:14 +0000

    refactor card to data driven component

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,75 @@
 <template>
 	<div class="hello">
+  <div class="topnav">
+<svg class="toiyp_logo-sml"
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="67.828697mm"
+   height="70.633896mm"
+   viewBox="0 0 67.828697 70.633896"
+   version="1.1"
+   id="svg8"
+   inkscape:version="0.92.2 5c3e80d, 2017-08-06"
+   sodipodi:docname="tioyp_logo.svg">
+  <defs
+     id="defs2" />
+  <sodipodi:namedview
+     id="base"
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1.0"
+     inkscape:pageopacity="0.0"
+     inkscape:pageshadow="2"
+     inkscape:zoom="2"
+     inkscape:cx="136.64832"
+     inkscape:cy="107.45253"
+     inkscape:document-units="mm"
+     inkscape:current-layer="layer1"
+     showgrid="false"
+     inkscape:snap-text-baseline="true"
+     inkscape:snap-grids="false"
+     inkscape:pagecheckerboard="true"
+     inkscape:window-width="1920"
+     inkscape:window-height="1052"
+     inkscape:window-x="0"
+     inkscape:window-y="0"
+     inkscape:window-maximized="1"
+     fit-margin-top="0"
+     fit-margin-left="0"
+     fit-margin-right="0"
+     fit-margin-bottom="0" />
+  <metadata
+     id="metadata5">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <g
+     inkscape:label="Layer 1"
+     inkscape:groupmode="layer"
+     id="layer1"
+     transform="translate(-86.308665,-114.67927)"
+     style="opacity:1">
+    <path
+       style="fill:#ffffff;fill-opacity:0.99468082;stroke:none;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
+       d="m 117.68015,114.6824 c -3.64029,0.11351 -6.25354,3.35295 -7.0559,4.47983 -45.54672,63.96831 -13.135115,64.98415 -13.135115,64.98415 7.761975,1.66099 26.481725,1.23161 37.309345,0.6413 17.96815,-0.82463 22.56119,-10.49217 17.28732,-25.34057 -4.45067,-7.4824 -9.50624,-12.26578 -14.0348,-15.37219 -1.84804,-1.26768 -3.58926,-2.24773 -5.16971,-3.0117 a 2.9352933,2.9352933 0 0 1 -0.18965,-0.0822 c -4.67425,-2.22336 -7.87084,-2.5275 -7.87084,-2.5275 a 2.9352933,2.9352933 0 0 1 -0.38292,-0.0605 c -7.87801,-0.80559 -15.99815,2.72994 -19.94142,10.00972 -4.47395,8.25946 -1.82529,19.29533 6.59391,23.69365 6.83129,3.56876 15.91664,1.32937 19.37711,-5.64669 2.67732,-5.39726 0.84983,-12.54041 -4.66793,-15.06626 -3.95146,-1.80885 -9.16242,-0.40728 -10.76213,3.62097 -0.49144,1.23763 -0.4965,2.83858 -0.031,4.07624 0.46549,1.23766 1.21859,2.05003 2.44895,2.40243 0.18029,0.0516 0.48199,-0.0619 0.76377,-0.0744 a 2.9352933,2.9352933 0 0 1 -0.55862,-3.92586 c 0.62534,-0.93979 1.97046,-1.86753 3.32228,-1.85363 1.35183,0.0139 2.23515,0.64825 2.82825,1.20509 1.18619,1.11367 1.86665,2.06709 1.82625,4.0628 -0.0535,2.64852 -1.87126,4.76382 -3.78892,5.69319 -1.91766,0.92936 -4.00291,1.11179 -6.00997,0.53692 -3.13337,-0.89749 -5.32504,-3.31854 -6.32623,-5.98051 -1.00119,-2.66198 -0.99437,-5.62759 0.0703,-8.30854 2.96772,-7.47307 11.8867,-9.89363 18.66036,-6.79286 8.77718,4.0179 11.54585,14.8201 7.48223,23.01203 -5.02229,10.12455 -17.74263,13.26266 -27.35337,8.24187 -11.494315,-6.00481 -15.009345,-20.66188 -9.035095,-31.69109 4.359495,-8.04817 12.479415,-12.58895 20.996125,-13.17232 5.11003,-0.35003 10.36344,0.7238 15.03112,3.32073 a 2.9352933,2.9352933 0 0 1 0.3514,0.22841 c 1.65682,0.80271 3.41066,1.74954 5.23638,2.96674 -4.3311,-6.27721 -9.63326,-12.9792 -15.77889,-19.96622 -2.75424,-3.31417 -5.30839,-4.3712 -7.49256,-4.3031 z"
+       id="path16"
+       inkscape:connector-curvature="0" />
+  </g>
+</svg>
+</div>
 <div class="container">
-
 <div class="navbar">
 <div class="navbar__header">
 <img class="avatar avatar-big" src="https://images.unsplash.com/photo-1476900966873-ab290e38e3f7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=fe0976a79ece0ee8effca4cab4527ae2">
@@ -40,54 +108,17 @@ Last Week</h4>
 </div>
 </div>
 <div class="main">
-<div class='component'>
-<section class='top'>
-</section>
-<div class="photo">
-<img class="avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"></img>
-</div>
-<h3 class="name">Jenny</h3>
-<h4 class="addr">Jen@toiyp.me</h4>
 
-<section class='bottom bottom--female'>
-</section>
-<div class="side-tab">
-<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
-<path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M54,64c0,0-6-5-6-12s0-40,0-40s0-11-8-11s-8,11-8,11
-	v40"/>
-<path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M16,52V12c0,0,0.083-11,8-11s8,11,8,11"/>
-<path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M16,12c0,0,0-10-8-10"/>
-<path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M48,24c0,0,0-14,6-14s6,14,6,14s-1,34-27,34"/>
-</svg>
-</div>
-
-
-</div>
-
-
-<div class='component'>
-<section class='top'>
-</section>
-<div class="photo">
-<img class="avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/peterme/128.jpg">
-</div>
-<h3 class="name">Rupert</h3>
-<h4 class="addr">RuMeister@toiyp.me</h4>
-<div class="side-tab">
-<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-	 width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
-<path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M41.667,38.002
-	c3.913-2.939,6.444-7.619,6.444-12.891C48.111,16.213,40.897,9,32,9s-16.111,7.213-16.111,16.111c0,5.27,2.53,9.948,6.442,12.889"/>
-<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="0" y1="38" x2="23" y2="38"/>
-<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="41" y1="38" x2="64" y2="38"/>
-<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="0" y1="55" x2="64" y2="55"/>
-</svg>
-</div>
-<section class='bottom bottom--male'>
-</section>
-
-
+<div v-for="friend in friends" track-by="addr">
+<wx-card class="component" :gender="friend.gender">
+<img :src="friend.picture" slot="avatar" class="avatar"/>
+<h3 slot="name">
+{{ friend.name }}
+</h3>
+<h4 slot="addr">
+{{ friend.addr}}
+</h4>
+</wx-card>
 </div>
 
 
@@ -107,11 +138,48 @@ Last Week</h4>
 <script>
 
 import '../assets/icons.svg';
+import card from './card'
 	export default {
   name: 'hello',
+  components: {
+    WxCard: card
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      friends: [
+        {
+          name: "Jenny",
+          addr: "jen@toiyp.me",
+          picture: "https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg",
+          gender: "female",
+          starsign: "",
+          love: 3
+        },
+        {
+          name: "Rupert",
+          addr: "RuMeister@toiyp.me",
+          picture: "https://s3.amazonaws.com/uifaces/faces/twitter/peterme/128.jpg",
+          gender: "male",
+          starsign: "",
+          love: 2
+        },
+         {
+          name: "James",
+          addr: "jim@toiyp.me",
+          picture: "https://s3.amazonaws.com/uifaces/faces/twitter/felipenogs/128.jpg",
+          gender: "male",
+          starsign: "",
+          love: 3
+        },
+         {
+          name: "Abbigale",
+          addr: "abby@toiyp.me",
+          picture: "https://images.unsplash.com/photo-1496081081095-d32308dd6206?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=dd302358c7e18c27c4086e97caf85781",
+          gender: "female",
+          starsign: "",
+          love: 1
+        }
+      ]
     }
   }
 }
@@ -129,10 +197,33 @@ import '../assets/icons.svg';
 
 .container {
 	display: grid;
-  height: 100vh;
+  height: 90vh;
   width: 100vw;
 	grid-template-columns: minmax(min-content, 200px) minmax(min-content, 200px) 1fr;
+  margin: 50px 10px;
+}
+
+.topnav {
+  background-color: orange;
+  color: white;
+  height: 15px;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0px 4px 20px rgba(0,0,0, .2);
+  position: fixed;
+  width: 100%;
+  z-index: 1000;
+  top: 0px;
   margin: 0px 10px;
+
+}
+
+.toiyp_logo-sml {
+  width: 35px;
+  position: absolute;
+  top: -103px;
+  left: 25px;
+  opacity: .7;
 }
 
 .navbar {
@@ -158,7 +249,7 @@ import '../assets/icons.svg';
 
 .timeline {
   grid-column: 2;
-  grid-row: 3 / -1;
+  grid-row: 2;
   max-width: 200px;
   margin-top: 15px;
   margin-left: 20px;
@@ -178,7 +269,7 @@ import '../assets/icons.svg';
   height: 30px;
   grid-row: 1;
   z-index: 1;
-  padding: 30px 5px;
+  padding: 25px 5px;
    clip-path: polygon(0 0, 0% 100%, 100% 85%, 100% 0%);
   //transform: rotate(-5deg) translateX(-10px) translateY(-10px);
 }
@@ -300,7 +391,7 @@ import '../assets/icons.svg';
   margin-right: 10px;
 } 
 
-.avatar {
+ .avatar {
   width: 100px;
   height: 100px;
   border: solid 1px white;
@@ -308,7 +399,7 @@ import '../assets/icons.svg';
   margin: 20px auto;
   transform: translateX(22px) rotate(-10deg);
   transition: all .6s;
-}
+} 
 
 .avatar-big {
   width: 100px;
@@ -319,8 +410,8 @@ import '../assets/icons.svg';
 }
 
 .handle {
-  transform: translateY(-27px);
-  color: black;
+  transform: translateY(-30px);
+  color: #333;
 }
 
 .icons {
@@ -344,12 +435,18 @@ svg {
 
 
 @media (max-width: 600px) {
+
+  .container {
+    margin: 0px 0px;
+  }
+
   .navbar {
     grid-row: 1;
     grid-column: 1 / -1;
     padding: 0px 0px;
     height: 70px;
     overflow: visible;
+    display: none;
   }
 
   .avatar-big {
@@ -363,16 +460,20 @@ svg {
   background-color: orange;
  height: 0%;
  visibility: none;
- 
  padding: 0px 0px;
+}
+
+.topnav {
+  margin: 0px;
 }
 
   .timeline {
      grid-row: 2;
    grid-column: 1 / -1;
-   height: 15px;
+   height: 35px;
    max-width: 85vw;
    z-index: -0;
+   
   }
 
 .main {
---
+++ src/components/card.vue
@@ -0,0 +1,154 @@
+<template>
+
+<div class='component'>
+<section class='top'>
+</section>
+<div class="photo">
+<div class="">
+<slot name="avatar">
+</slot>
+</div>
+</div>
+<div class="name">
+<slot name="name">
+</slot>
+</div>
+<div class="addr">
+<slot name="addr">
+</slot>
+</div>
+
+<div class="side-tab">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="64px" height="64px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" xml:space="preserve">
+<path fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" d="M41.667,38.002
+	c3.913-2.939,6.444-7.619,6.444-12.891C48.111,16.213,40.897,9,32,9s-16.111,7.213-16.111,16.111c0,5.27,2.53,9.948,6.442,12.889"/>
+<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="0" y1="38" x2="23" y2="38"/>
+<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="41" y1="38" x2="64" y2="38"/>
+<line fill="none" stroke="#000000" stroke-width="2" stroke-miterlimit="10" x1="0" y1="55" x2="64" y2="55"/>
+</svg>
+</div>
+
+<div v-if="gender!=='female'" class="bottom bottom--male" >
+</div>
+<div v-else class="bottom">></div>
+</div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+ props: [ 'gender' ]
+}
+</script>
+
+<style>
+.component{
+  background-color: #fff;
+  height: 150px;
+  width: 100%;
+  border-radius: 5px;
+  box-shadow: 0px 4px 20px rgba(0,0,0, .2);
+  transition: all .2s;
+  border: 1px solid peachpuff;
+
+  margin: 5px;
+  text-align: left;
+  display: grid;
+  grid-template-columns:  170px minmax(min-content, 1fr) minmax(min-content, 1fr);
+  grid-template-rows: 20% 20% [bottom-start] 20% 20% 20% [bottom-end];
+  grid-gap: 5px;
+  
+  overflow: hidden;
+}
+
+.component:hover {
+  transform: scale(1.05);
+}
+
+.component:active {
+  transform: rotateY(90deg);
+}
+
+.bottom {
+  grid-row: 4;
+  grid-column-start: 1;
+  grid-column-end: 5;
+  height: 80px;
+  width: 130%;
+  display: block;
+  background-color:blueviolet;
+  
+  padding: 10px;
+  transform: rotate(-5deg) translateY(-15px);
+  opacity: .5;
+}
+
+.side-tab {
+ background-color: rgba(250,115,50, .5);
+ border-left: 1px solid white;
+ grid-row: 1 / -1;
+ width: 50px;
+ z-index: 200;
+ float: right;
+ text-align: center;
+}
+
+ .bottom--male {
+    background-color: lightskyblue;
+  }
+
+.top {
+  grid-row: 1 / 6;
+  grid-column-start: 1;
+  grid-column-end: 5;
+  background-color: white;
+}
+
+.addr {
+  font-size: 12px;
+  line-height: 2.5;
+  grid-row: 2;
+  grid-column: 2; 
+  color: #444;
+  font-weight: 300;
+
+}
+
+svg {
+  width: 20px;
+}
+
+.name {
+  grid-row: 2;
+  grid-column: 2; 
+  color: #444;
+  transform: translateY(-20px);
+  
+}
+
+.photo {
+  grid-column: 1 / span 2;
+  grid-row-start: 1;
+  width: 150px;
+  height: 300px;
+  background-color: orange;
+  transform: rotate(10deg) translateY(15px) translateX(-10px);
+  z-index: 99;
+  border-radius: 5px;
+  margin-right: 10px;
+} 
+
+.avatar {
+  width: 100px;
+  height: 100px;
+  border: solid 1px white;
+  border-radius: 50%;
+  margin: 20px auto;
+  transform: translateX(22px) rotate(-10deg);
+  transition: all .6s;
+}
+
+</style>
